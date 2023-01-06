39f8d8d50e9dd4f84479fe018fe1ce72abb185f0
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2023-01-06 10:10:10 -0300

    03.02_Criando_Model_Endereco

--- app/models.py
+++ app/models.py
@@ -2,6 +2,40 @@ from django.db import models
 
 # Create your models here.
 
+class Endereco(models.Model):
+    ESTADO_CHOICES = (
+        ('AC', 'Acre'), 
+        ('AL', 'Alagoas'),
+        ('AP', 'Amapá'),
+        ('BA', 'Bahia'),
+        ('CE', 'Ceará'),
+        ('DF', 'Distrito Federal'),
+        ('ES', 'Espírito Santo'),
+        ('GO', 'Goiás'),
+        ('MA', 'Maranão'),
+        ('MG', 'Minas Gerais'),
+        ('MS', 'Mato Grosso do Sul'),
+        ('MT', 'Mato Grosso'),
+        ('PA', 'Pará'),
+        ('PB', 'Paraíba'),
+        ('PE', 'Pernanbuco'),
+        ('PI', 'Piauí'),
+        ('PR', 'Paraná'),
+        ('RJ', 'Rio de Janeiro'),
+        ('RN', 'Rio Grande do Norte'),
+        ('RO', 'Rondônia'),
+        ('RR', 'Roraima'),
+        ('RS', 'Rio Grande do Sul'),
+        ('SC', 'Santa Catarina'),
+        ('SE', 'Sergipe'),
+        ('SP', 'São Paulo'),
+        ('TO', 'Tocantins')
+    )
+    rua = models.CharField(max_length=100, null=False, blank=False)
+    numero = models.IntegerField(null=False, blank=False)
+    bairro = models.CharField(max_length=100, null=False, blank=False)
+    cidade = models.CharField(max_length=100, null=False, blank=False)
+    estado = models.CharField(choices=ESTADO_CHOICES, max_length=2, null=False, blank=False)
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
