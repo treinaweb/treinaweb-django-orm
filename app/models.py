767b5dc6c0984ae4e0cac5bb4406cb0ebbb0bee1
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2023-01-06 10:20:06 -0300

    03.03_Relacionando_Entidades_Endereco_Cliente

--- app/models.py
+++ app/models.py
@@ -42,3 +42,4 @@ class Cliente(models.Model):
     data_nascimento = models.DateField(null=False, blank=False)
     email = models.EmailField(null=False, blank=False)
     profissao = models.CharField(max_length=100, null=False, blank=False)
+    endereco = models.OneToOneField(to=Endereco, on_delete=models.SET_NULL, null=True)
--- clientes/settings.py
+++ clientes/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = "clientes.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "clientes",
+        "NAME": "clientes_2",
         "USER": "root",
         "PASSWORD": "root",
         "HOST": "localhost",
